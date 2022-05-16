86a50f5a38b64fc116ecd6b48ebdc3b5cc3acad2
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-16 10:02:39 +0800

    [refactor] Rename about.xml, mod namespace and classnames so that we can use this mod as a base for Autoharvest Wild Crops

--- Source/AllowDeadAnimalsMod.cs
+++ Source/AllowDeadAnimalsMod.cs
@@ -2,40 +2,40 @@
 using Verse;
 using RimWorld;
 
-namespace AllowDeadAnimals
+namespace Daffodilistic.RimWorld.AutoHarvestWildPlants
 {
-	public class AllowDeadAnimalsMod : Mod
-	{
+    public class AutoHarvestWildPlantsMod : Mod
+    {
 
-		AllowDeadAnimalsModSettings _settings;
+        AutoHarvestWildPlantsModSettings _settings;
 
-		public AllowDeadAnimalsMod ( ModContentPack content )
-			: base( content )
-			=> this._settings = GetSettings<AllowDeadAnimalsModSettings>();
+        public AutoHarvestWildPlantsMod(ModContentPack content)
+            : base(content)
+            => this._settings = GetSettings<AutoHarvestWildPlantsModSettings>();
 
-		public override string SettingsCategory () => "Allow Dead Animals";
+        public override string SettingsCategory() => "Autoharvest Wild Crops";
 
-		public override void DoSettingsWindowContents ( Rect rect )
-		{
-			var listing = new Listing_Standard();
-			listing.Begin(rect);
-			{
-				listing.CheckboxLabeled( "Allow" , ref _settings.allow , "Is auto-allow enabled?" );
-				listing.CheckboxLabeled( "Notify" , ref _settings.notify , "Are notifications enabled?" );
+        public override void DoSettingsWindowContents(Rect rect)
+        {
+            var listing = new Listing_Standard();
+            listing.Begin(rect);
+            {
+                listing.CheckboxLabeled("Allow", ref _settings.allow, "Is auto-allow enabled?");
+                listing.CheckboxLabeled("Notify", ref _settings.notify, "Are notifications enabled?");
 
-				var section = listing.BeginSection(24*4);
-					section.CheckboxLabeled( "Animal" , ref _settings.allowAnimal );
-					section.CheckboxLabeled( "Insect" , ref _settings.allowInsect );
-					section.CheckboxLabeled( "Humanlike" , ref _settings.allowHumanlike );
-					section.CheckboxLabeled( "Mechanoid" , ref _settings.allowMechanoid );
-				listing.EndSection( section );
+                var section = listing.BeginSection(24 * 4);
+                section.CheckboxLabeled("Animal", ref _settings.allowAnimal);
+                section.CheckboxLabeled("Insect", ref _settings.allowInsect);
+                section.CheckboxLabeled("Humanlike", ref _settings.allowHumanlike);
+                section.CheckboxLabeled("Mechanoid", ref _settings.allowMechanoid);
+                listing.EndSection(section);
 
-				string mass_threshold_label =_settings.mass_threshold.ToString();
-				listing.TextFieldNumericLabeled( "Ignore corpses lighter than:" , ref _settings.mass_threshold , ref mass_threshold_label );
-			}
-			listing.End();
-			base.DoSettingsWindowContents(rect);
-		}
+                string mass_threshold_label = _settings.mass_threshold.ToString();
+                listing.TextFieldNumericLabeled("Ignore corpses lighter than:", ref _settings.mass_threshold, ref mass_threshold_label);
+            }
+            listing.End();
+            base.DoSettingsWindowContents(rect);
+        }
 
-	}
+    }
 }
