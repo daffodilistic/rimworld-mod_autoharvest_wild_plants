0d44135d7c778a0e6ab74f9a9f62eff5a33f48b8
Soh Thiam Hing <daffodilistic@gmail.com>
2022-05-22 15:57:17 +0800

    [chore] Removed unused settings

--- Source/AutoHarvestWildPlantsMod.cs
+++ Source/AutoHarvestWildPlantsMod.cs
@@ -22,16 +22,6 @@ namespace Daffodilistic.RimWorld.AutoHarvestWildPlants
             {
                 listing.CheckboxLabeled("Allow", ref _settings.allow, "Is auto-allow enabled?");
                 listing.CheckboxLabeled("Notify", ref _settings.notify, "Are notifications enabled?");
-
-                var section = listing.BeginSection(24 * 4);
-                section.CheckboxLabeled("Animal", ref _settings.allowAnimal);
-                section.CheckboxLabeled("Insect", ref _settings.allowInsect);
-                section.CheckboxLabeled("Humanlike", ref _settings.allowHumanlike);
-                section.CheckboxLabeled("Mechanoid", ref _settings.allowMechanoid);
-                listing.EndSection(section);
-
-                string mass_threshold_label = _settings.mass_threshold.ToString();
-                listing.TextFieldNumericLabeled("Ignore corpses lighter than:", ref _settings.mass_threshold, ref mass_threshold_label);
             }
             listing.End();
             base.DoSettingsWindowContents(rect);
