abb45d4699cabdcfd00b5499dd730ae14ca41f33
Soh Thiam Hing <daffodilistic@gmail.com>
2022-12-26 13:07:42 +0800

    Use LogMessage() for easier toggling between debug/release builds

--- Source/AutoHarvestWildPlantsMapComponent.cs
+++ Source/AutoHarvestWildPlantsMapComponent.cs
@@ -35,10 +35,17 @@ namespace Daffodilistic.RimWorld.AutoHarvestWildPlants
             }
         }
 
+        private static void LogMessage(string message)
+        {
+#if DEBUG
+            Log.Message("[AutoHarvestWildPlants] " + message);
+#endif
+        }
+
         /// <remarks> Things returned from ThingsInGroup() is NOT thread-safe so EXPECT it can be changed by diffent CPU thread, mid-execution, anytime here.</remarks>
         void AutoHarvestWildPlants()
         {
-            // Log.Message("[AutoHarvestWildPlants] AutoHarvestWildPlants() BEGIN");
+            LogMessage("[AutoHarvestWildPlants] AutoHarvestWildPlants() BEGIN");
             Faction playerFaction = Faction.OfPlayer;
             float massThreshold = _settings.mass_threshold;
             int ticksGame = _tickManager.TicksGame;
@@ -53,7 +60,7 @@ namespace Daffodilistic.RimWorld.AutoHarvestWildPlants
             if (!allow && !notify) return;// lets not fill _allowedAlready for nothing
 
             var list = map.listerThings.ThingsInGroup(ThingRequestGroup.HarvestablePlant);
-            // Log.Message("[AutoHarvestWildPlants] AutoHarvestWildPlants() list.Count:" + list.Count);
+            LogMessage("[AutoHarvestWildPlants] AutoHarvestWildPlants() list.Count:" + list.Count);
             for (int i = 0; i < list.Count; i++)
             {
                 if (
@@ -81,12 +88,12 @@ namespace Daffodilistic.RimWorld.AutoHarvestWildPlants
                     Messages.Message(text: "RipePlantSpotted".Translate((NamedArgument)wildPlant.LabelShort), lookTargets: wildPlant, def: MessageTypeDefOf.NeutralEvent);
                 }
             }
-            // Log.Message("[AutoHarvestWildPlants] AutoHarvestWildPlants() END");
+            LogMessage("[AutoHarvestWildPlants] AutoHarvestWildPlants() END");
         }
 
         bool validHarvestTarget(Plant plant)
         {
-            // Log.Message("[AutoHarvestWildPlants] validHarvestTarget() wildPlant: " + plant.Label +
+            // LogMessage("[AutoHarvestWildPlants] validHarvestTarget() wildPlant: " + plant.Label +
             // "\nPurpose:" + plant.def.plant.purpose.ToString().ToLower());
 
             bool isFood = plant.def.plant.purpose.ToString().ToLower() == "food";
